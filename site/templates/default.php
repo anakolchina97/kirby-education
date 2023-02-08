e4037d80a4d6fb498cf86a52ebef77adcd4c496f
Annie <anniehartmann97@gmail.com>
2023-02-08 11:44:39 +0300

    Add styles, foreach

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,2 +1,24 @@
-<h1><?= $page->title() ?></h1>
-<p><?= $page->text() ?></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $site->title() ?></title>
+  <?= css('assets/css/index.css') ?>
+</head>
+<body>
+  <header class="header">
+    <a class="logo" href="<?= $site->url() ?>"><?= $site->title() ?></a>
+    <nav>
+      <ul>
+        <?php foreach ($site->children() as $item): ?>
+          <li><a href="<?= $item->url() ?>"><?= $item->title() ?></a></li>
+        <?php endforeach; ?>
+      </ul>
+    </nav>
+  </header>
+  <h1><?= $page->title() ?></h1>
+  <p><?= $page->text() ?></p>
+</body>
+</html>
