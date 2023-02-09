9031c804c373ef1572dec10dbac5503c24a8632b
Annie <anniehartmann97@gmail.com>
2023-02-09 16:02:06 +0300

    Add filter

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -1,18 +1,35 @@
 <?php snippet('header') ?>
 <?php
-  $projects = $page->children()->listed()->paginate(1);
+  $filterBy = get('filter');
+
+  $unfiltered = $page->children()->listed();
+  $projects = $unfiltered->when($filterBy, function($filterBy) {
+    return $this->filterBy('category', $filterBy);
+  })->paginate(1);
   $pagination = $projects->pagination();
+  $filters = $unfiltered->pluck('category', null, true)
 ?>
   <main>
     <div class="container">
       <h1 class="title"><?= $page->title() ?></h1>
+
+      <nav class="filter">
+        <a class="filter__item" href="<?= $page->url() ?>">All</a>
+        <?php foreach ($filters as $filter): ?>
+          <a class="filter__item" href="<?= $page->url() ?>?filter=<?= $filter ?>"><?= $filter ?></a>
+        <?php endforeach ?>
+      </nav>
+
       <ul class="projects">
         <?php foreach ($projects as $project): ?>
           <li class="menu__item">
             <a class="menu__link" href="<?= $project->url() ?>">
               <figure>
                 <?= $project->image()->crop(300) ?>
-                <figcaption><?= $project->title() ?></figcaption>
+                <figcaption>
+                  <?= $project->title() ?><br>
+                  <small><?= $project->category() ?></small>
+                </figcaption>
               </figure>
             </a>
           </li>
